2a24b89dd491dcf14d3456c2e202ad7889f4d324
rupesh <rpiwal02@gmail.com>
2024-12-31 16:02:06 +0530

    theme store & setting page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,9 +8,11 @@ import Login from "./pages/Login";
 import Settings from "./pages/Settings";
 import { useAuthStore } from "./store/useAuthStore";
 import { Toaster } from "react-hot-toast";
+import { useThemeStore } from "./store/useThemeStore";
 
 const App = () => {
   const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+  const { theme } = useThemeStore();
 
   useEffect(() => {
     checkAuth();
@@ -26,7 +28,7 @@ const App = () => {
     );
 
   return (
-    <div>
+    <div data-theme={theme}>
       <Navbar />
       <Routes>
         <Route
