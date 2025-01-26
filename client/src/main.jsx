6b488f0ffb51bb71c39a7d6a88844ec1ffd427c3
rupesh <rpiwal02@gmail.com>
2025-01-26 15:34:13 +0530

    dependency error fix

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,5 @@
-import "@fontsource/inter/400.css"; // Regular weight
-import "@fontsource/inter/500.css"; // Medium weight
-import "@fontsource/inter/600.css"; // Semibold
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/500.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
