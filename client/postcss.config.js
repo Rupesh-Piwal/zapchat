dd5ef828596a3a70d055043852d7200529e6d4b4
rupesh <rpiwal02@gmail.com>
2025-01-26 15:23:02 +0530

    import  changes

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,7 +1,7 @@
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
+import daisyui from "daisyui";
+
 export default {
-  plugins: [
-    require("tailwindcss"),
-    require("autoprefixer"),
-    require("daisyui"), 
-  ],
+  plugins: [tailwindcss, autoprefixer, daisyui],
 };
